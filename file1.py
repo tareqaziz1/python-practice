8b852ccbca6108f4e80cdc3bd496707bd1692977
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-10 23:34:10 +0100

    initial commit

---
+++ file1.py
@@ -0,0 +1,14 @@
+price = 100
+print(price)
+
+rating = 4.5
+print(rating)
+name = "Tareq Aziz"
+print(name)
+is_True = False
+print(is_True)
+
+first_name = 'Tareq'
+last_name = 'Aziz'
+full_name = ("This is " + first_name + " " + last_name)
+print(full_name)
