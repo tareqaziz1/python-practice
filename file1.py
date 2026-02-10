4aca54d317e9f21f440a20b04f0a5239fb5d6238
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-11 00:37:18 +0100

    updated file1.py

--- file1.py
+++ file1.py
@@ -12,3 +12,15 @@ first_name = 'Tareq'
 last_name = 'Aziz'
 full_name = ("This is " + first_name + " " + last_name)
 print(full_name)
+
+#how to take input
+
+inputName = input("What's your name? ")
+
+print(inputName)
+
+#how to take number input
+
+inputNumber = int(input("What is your age? "))
+
+print(inputNumber)
