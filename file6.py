e485247f316b322ba2361907c8ebedf60df3246c
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-11 22:49:08 +0100

    file6 added

--- file6.py
+++ file6.py
@@ -13,6 +13,7 @@ print("Have a good day!!") # it will be executed without any condition
     Otherwise 5% discount will be given. Pring the payment.
 '''
 
+''''
 is_returning_customer = True
 price = 100
 
@@ -23,5 +24,26 @@ else:
     customer_price = price - (price * 0.05)
     print(customer_price)
 print("Check the price above")
+''
 # When is_returning_customer = True outcome is 90
 # When is_returning_customer = False outcome is 95
+
+#using elif for 2 or more conditions
+
+If the customar is older than 50 he gets 20% discount. Print the price.'''
+
+is_returning_customer = False
+price = 100
+customer_age = 51
+
+if is_returning_customer:
+    discounted_price = price - (price * 0.1)
+    print(discounted_price)
+
+elif customer_age > 50:
+    discounted_price = price - (price * 0.2)
+    print(discounted_price)
+
+else:
+    customer_price = price - (price * 0.05)
+    print(customer_price)
