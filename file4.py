67965a56c9df690ecee2f6166260a49d8e969552
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-11 22:04:39 +0100

    file4 updated

--- file4.py
+++ file4.py
@@ -1,6 +1,25 @@
 #Arithmetic Operations
+
 ''''
-print(taru)
-
-
+to add +
+to substract -
+to multiply *
+to divide /
+to get full integer after division // (10//3) = 3
+to get the reminder % (modulus) (10%3) = 1
+to add a power ** (10**3) = 1000
+assignment operator += or -=
+x+=1 / x-= 1 ( Increase and decrease )
 '''
+
+# Round function
+
+x = 4.9
+x_round = round(x)
+print(x_round) # output = 5
+
+# Absolute function
+
+y = -5
+y_absolute = abs(y)
+print(y_absolute) #output = 5
