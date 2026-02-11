a44af5dd345065d2ca616445d97d3331a0102e7b
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-11 22:12:40 +0100

    file5 added

---
+++ file5.py
@@ -0,0 +1,13 @@
+# importing a module
+
+import math
+
+x = 3.8777
+y = math.ceil(x)
+print(y) #output = 4
+
+z = math.floor(x)
+print(z) #output = 3
+
+fac = math.factorial(4)
+print(fac) #output = 24
