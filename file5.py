ce55e3e74cf7aa0c41264b31fe2d95984c23ceaf
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-11 22:15:38 +0100

    file5 updated

--- file5.py
+++ file5.py
@@ -11,3 +11,16 @@ print(z) #output = 3
 
 fac = math.factorial(4)
 print(fac) #output = 24
+
+sqrt = math.sqrt(64)
+print(sqrt) #output = 8
+
+''''
+sqrt(x) : Computes the square root of x.
+pow(x, y) : Raises x to the power of y.
+exp(x) : Returns e^x.
+log(x, base) : Computes the logarithm of x to the specified base.
+sin(x) , math. cos(x) , math. ...
+degrees(x) , math. ...
+gcd(a, b) : Computes the greatest common divisor.
+'''
