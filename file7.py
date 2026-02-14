7fb92447567d258a90d59f29d5da2c43bb28db37
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-14 03:34:26 +0100

    file7 added

---
+++ file7.py
@@ -0,0 +1,23 @@
+# Logical operators (and, or, not)
+
+'''if a candidate has high score and skills he gets the job'''
+
+has_high_score = True
+has_good_skills = False
+#logical AND
+if has_high_score and has_good_skills:
+    print("He is eligible for the job!")
+else:
+    print("He is not eligible for the job!")
+
+'''if a candidate has good score OR good skills he can get an internship.'''
+#logical OR
+if has_high_score or has_good_skills:
+    print("He is eligible for an intership")
+else:
+    print("He is not eligible for anything!")
+
+#logical NOT (reverse the boolean value)
+
+if has_high_score and not has_good_skills:
+    print("Boolean value is reversed")
