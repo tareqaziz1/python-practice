724e40c9cd6d58275d16dd97012608db08d59a5c
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-15 04:01:04 +0100

    file8 updated

--- file8.py
+++ file8.py
@@ -8,9 +8,25 @@ while i <= 5:
 print("finished")
 
 
-#
+# Print half pyramid with asterisks
 j = 1
 while j <= 5:
+    print(j * "*")
     j += 1
-    print(j * "*" )
+
+# Make a 6 row pyramid
+
+row = 6
+z = 1
+
+while z <= row:
+    spaces = row - z
+    stars = 2 * z - 1
+
+    print(" " * spaces + "*" * stars)
+    z += 1
+
+
+
+
 
