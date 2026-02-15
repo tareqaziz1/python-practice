1b99cc5f0d095d6e83f4b60ab2a5aa5240222541
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-15 04:21:45 +0100

    file8 updated

--- file8.py
+++ file8.py
@@ -26,6 +26,28 @@ while z <= row:
     print(" " * spaces + "*" * stars)
     z += 1
 
+# Guessing game with 3 attempt
+
+
+secret_number = 7
+guess = 0
+guess_limit = 3
+
+while guess < guess_limit:
+    input_number = int(input("What is the secret number? : "))
+    guess += 1
+    if secret_number == input_number:
+        print("Your guess is correct!!!")
+        print("Thank you for playing.")
+        break
+    elif guess == guess_limit:
+        print("Your guess is wrong. Try again !")
+        print("Thank you for playing.")
+    else:
+        print("Your guess is wrong. Try again !")
+
+# car engine program
+
 
 
 
