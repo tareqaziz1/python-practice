0567285faa167871707efaa0d9047933a54e92ed
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-16 02:09:08 +0100

    file9 added

---
+++ file9.py
@@ -0,0 +1,20 @@
+# for loop
+
+for i in "python":
+    print(i)
+
+#to print a range of 1 to 9 (range has to be until 10) -
+
+for x in range(10):
+    print(x)
+
+for z in range(20, 31):
+    print(z) # it will print 20 to 30
+
+# List is a list of items. It can be numbers or strings. Syntax - []
+
+for item in ["Tareq", "John", "Tom"]:
+    print(item)
+
+for number in [1,2,3,4,5,6]:
+    print(number)
