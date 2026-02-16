d90ab23432079da228b80ac858bb7f5d82a870d8
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-16 04:33:05 +0100

    file10 updated

--- file10.py
+++ file10.py
@@ -8,5 +8,50 @@ print(names[-2]) # output - Sofia
 print(names[2:]) # All items index from 2 ['Sofia', 'Sarah']
 print(names[1:4]) #index 1 to 3 ['Tom', 'Sofia', 'Sarah']
 
+# Accessing with index does not change the original list.
 
+# Modifying list
 
+names[0] = "John Smith"
+print(names) #output - ['John Smith', 'Tom', 'Sofia', 'Sarah']
+
+# Find the largest number from the list
+
+numbers = [10,15,13,20,14,16,17,19,18]
+
+largest_number = numbers[0]
+
+for i in numbers:
+    if i > largest_number:
+        largest_number = i
+
+print(largest_number)
+
+# 2D list
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+# How to find 5 from the 2nd items of the list
+
+print(matrix[1][1]) #output - 5
+
+# How to find 7 from the list?
+
+print(matrix[2][0])
+
+# modify the value 9 to 10
+
+matrix[2][2] = 10
+print(matrix[2][2]) #output - 10
+
+print(matrix) #output - [[1, 2, 3], [4, 5, 6], [7, 8, 10]]
+
+#Get all the numbers from the 2D list
+
+for row in matrix:
+    for items in row:
+        print(items)
