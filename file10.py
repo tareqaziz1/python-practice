02e66342f40fd668d7c299310d33ed64d83923dc
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-16 03:57:42 +0100

    file10 added

---
+++ file10.py
@@ -0,0 +1,12 @@
+# All about List []
+
+names = ["John", "Tom", "Sofia", "Sarah"]
+print(names[0]) # output- John
+print(names[1]) # output- Tom
+print(names[-1]) # output- Sarah
+print(names[-2]) # output - Sofia
+print(names[2:]) # All items index from 2 ['Sofia', 'Sarah']
+print(names[1:4]) #index 1 to 3 ['Tom', 'Sofia', 'Sarah']
+
+
+
