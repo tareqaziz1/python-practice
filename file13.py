ba94c5d6cb06e54cc1d98566f6a425e318f80e8a
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-17 05:49:36 +0100

    file13 added

---
+++ file13.py
@@ -0,0 +1,12 @@
+#Dictionary
+# Dictionary is used to store information that comes as key value pairs.
+# key always should be unique. key below - name, email and phone.
+customar = {
+    "name": "John Smith",
+    "email": "john@gmail.com",
+    "phone": 123456789,
+    "is_varified": True,
+}
+
+name = customar["name"]
+print(name)
