684b7bf9cc33a521c5590e2635b363e4476b0e09
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-22 21:39:56 +0100

    file14 updated

--- file14.py
+++ file14.py
@@ -1,2 +1,34 @@
 # Functions
 
+# Make a function that prints "Hello World"
+
+def hello_world():
+    print("Hello World")
+
+hello_world()
+
+# Make function with parameters
+
+def greeting(name):
+    print(f"Good morning {name}")
+
+greeting("John")
+greeting("Sarah")
+
+
+def custom_greeting(first_name, last_name, message):
+    print(f"Hey, {first_name} {last_name}, {message}")
+
+custom_greeting("Tareq", "Aziz", "Welconme aboard!")
+
+# Keyword argument ( position does not matter if it is used)
+
+custom_greeting(last_name="Aziz", message="Good evening", first_name="Tareq")
+
+
+
+
+
+
+
+
