132e040d026352af12c1e1c26be839fcb45cd76f
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-24 04:51:15 +0100

    file16 added

---
+++ file16.py
@@ -0,0 +1,14 @@
+# Class
+
+''' We use classes to define types.'''
+'''
+A class is like a blueprint for creating objects
+because it defines the structure and behavior that 
+every object created from it will have. 
+Just like a blueprint of a house shows 
+what rooms, doors, and windows the house will have, 
+a class describes what data (attributes) and actions (methods) an object should contain. 
+When you create an object from a class, you are making a real instance based on that design, 
+with its own values but the same structure. 
+This makes code more organized, reusable, and easier to manage as programs grow.
+'''
