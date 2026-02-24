5a22916aa1fa6653062ceb3e0224af643e64b64d
tareqaziz1 <tareqazeez1@gmail.com>
2026-02-24 04:25:45 +0100

    file15 updated

--- file15.py
+++ file15.py
@@ -1,1 +1,17 @@
 # Exeptions (try & except)
+
+#try is used to run code that might fail or cause an error.
+#If something goes wrong, except catches the error so the program doesn’t crash.
+
+try:
+    age = int(input("Age: "))
+    income = 1000
+    risk = income / age
+    print(f"Age is {age} years")
+    print(f"Risk is {risk}")
+except ValueError:
+    print("value should be a number!")
+except ZeroDivisionError:
+    print("Age cannot be 0!")
+
+
