f31bac477c222b046bfac4a80ff567e78d3edce9
tareqaziz1 <tareqazeez1@gmail.com>
2026-03-12 23:44:03 +0100

    file19 updated

--- file19.py
+++ file19.py
@@ -3,7 +3,12 @@
 #Absolute path : The full path to a file or folder starting from the root directory.
 #Relative path : The path to a file or folder based on the current working directory.
 
-
-
-
 from pathlib import Path
+
+path = Path()
+print(path.exists()) #False
+
+for file in path.glob('*.py'):
+    print(file)                 #shows all .py files
+
+
